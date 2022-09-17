5cfc4f7bec2be7717bacd154fe61f854f16c3ad4
Milan Biranwar <milanbiranwar06@gmail.com>
2022-09-17 14:50:09 +0530

    Program for calculating total EMP Wage in UC6

--- EmpWage.js
+++ EmpWage.js
@@ -118,7 +118,7 @@ let empWage = totalEmpHrs*WAGE_PER_HOUR;
 console.log("UC5 - total days : " + totalWorkingDays + " Total Hrs : " + totalEmpHrs +  "  EmpWage : " + empWage);
 
 */
-
+/*
 //UC5
 function getWorkingHours(empCheck)
 {
@@ -163,4 +163,53 @@ empCheck = Math.floor(Math.random()*10)%3;
 empHrs = getWorkingHours(empCheck);
 let empWage = totalEmpHrs*WAGE_PER_HOUR;
 console.log("UC5 - total days : " + totalWorkingDays + " Total Hrs : " + totalEmpHrs +  "  EmpWage : " + empWage);
+
+*/
+
+
+
+//UC6
+function getWorkingHours(empCheck)
+{
+    switch(empCheck)
+    {
+        case IS_PART_TIME:
+        return PART_TIME_HOURS;
+        
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        
+        default:
+            return 0;
+    }
+}
+
+function calacDailyWage(empHrs)
+{
+    return empHrs* WAGE_PER_HOUR;
+}
+
+const IS_PART_TIME =1;
+const IS_FULL_TIME =2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+const MAX_HRS_IN_MONTH = 160;
+const Num_OF_WORKING_DAYS = 20;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+let empDailyWageArr = new Array();
+
+
+while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<Num_OF_WORKING_DAYS)
+{
+    totalWorkingDays++;
+    empCheck=Math.floor(Math.random()*10)%3;
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs+=empHrs;
+    empDailyWageArr.push(calacDailyWage(empCheck));
+}
+
+let empWage = calacDailyAage(totalEmpHrs);
+console.log("UC6 - Total days : " + totalWorkingDays + " Total Hrs : " + totalEmpHrs +  "  Emp Wage : " + empWage);
 
