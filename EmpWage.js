675f58b08aae13b6077cf3818199e2ed556f8ae0
Milan Biranwar <milanbiranwar06@gmail.com>
2022-09-17 14:47:09 +0530

    Program for calculating total EMP Wage

--- EmpWage.js
+++ EmpWage.js
@@ -47,7 +47,7 @@ console.log("Emp Wage : " + empWage);
 */
 
 
-
+/*
 //UC3
 function getWorkingHours(empCheck)
 {
@@ -75,3 +75,45 @@ empCheck = Math.floor(Math.random()*10)%3;
 empHrs = getWorkingHours(empCheck);
 let empWage = empHrs*WAGE_PER_HOUR;
 console.log("Emp Wage: " + empWage);
+*/
+
+
+//UC4
+function getWorkingHours(empCheck)
+{
+    switch(empCheck)
+    {
+        case IS_PART_TIME:
+        return PART_TIME_HOURS;
+        
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        
+        default:
+            return 0;
+    }
+}
+
+const IS_PART_TIME =1;
+const IS_FULL_TIME =2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+const MAX_HRS_IN_MONTH = 100;
+const Num_OF_WORKING_DAYS = 10;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+
+
+while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<Num_OF_WORKING_DAYS)
+{
+    totalWorkingDays++;
+    empCheck=Math.floor(Math.random()*10)%3;
+    totalEmpHrs += getWorkingHours(empCheck);
+}
+
+empCheck = Math.floor(Math.random()*10)%3;
+empHrs = getWorkingHours(empCheck);
+let empWage = totalEmpHrs*WAGE_PER_HOUR;
+console.log("UC5 - total days : " + totalWorkingDays + " Total Hrs : " + totalEmpHrs +  "  EmpWage : " + empWage);
+
