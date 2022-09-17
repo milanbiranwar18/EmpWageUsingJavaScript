8cb1a27071c3bf39f2351f27c727916a4147999c
Milan Biranwar <milanbiranwar06@gmail.com>
2022-09-17 14:44:38 +0530

    Program for calculating Emp Wage with part time

--- EmpWage.js
+++ EmpWage.js
@@ -22,7 +22,7 @@ else{
 
 
 //UC2
-
+/*
 const IS_PART_TIME =1;
 const IS_FULL_TIME =2;
 const PART_TIME_HOURS = 4;
@@ -44,5 +44,34 @@ switch(empCheck)
 }
 let empWage = empHrs*WAGE_PER_HOUR;
 console.log("Emp Wage : " + empWage);
+*/
 
 
+
+//UC3
+function getWorkingHours(empCheck)
+{
+    switch(empCheck)
+    {
+        case IS_PART_TIME:
+        return PART_TIME_HOURS;
+        
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        
+        default:
+            return 0;
+    }
+}
+
+const IS_PART_TIME =1;
+const IS_FULL_TIME =2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+let empHrs = 0;
+
+empCheck = Math.floor(Math.random()*10)%3;
+empHrs = getWorkingHours(empCheck);
+let empWage = empHrs*WAGE_PER_HOUR;
+console.log("Emp Wage: " + empWage);
